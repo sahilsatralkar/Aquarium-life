17eea483220377230fa7e2f79cfec1e16e94ac9f
Sahil Satralkar <sahil.satralkar@gmail.com>
2021-08-07 13:29:58 +0530

    Added new Unit test target

---
+++ Aquarium lifeTests/Aquarium_lifeTests.swift
@@ -0,0 +1,32 @@
+//
+//  Aquarium_lifeTests.swift
+//  Aquarium lifeTests
+//
+//  Created by Sahil Satralkar on 07/08/21.
+//
+
+import XCTest
+
+class Aquarium_lifeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
